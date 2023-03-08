b9738c32d81c392d948fc4f65741684bb0c94533
bulbuz <63503381+bulbuz@users.noreply.github.com>
2023-03-08 22:08:40 +0100

    Add files via upload

---
+++ gen.py
@@ -0,0 +1,75 @@
+import requests
+import sys
+import os
+import pickle
+from bs4 import BeautifulSoup
+
+settings = {
+    "PROBLEM_ID": None,
+    "single case": True,
+    "case cnt": 1,
+}
+
+CONTEST_ID = sys.argv[1] # contest id (not contest number)
+PROBLEM_ID = sys.argv[2].capitalize() # problem letter (TODO: make it optional)
+
+PATH = os.getcwd()
+
+url = "https://www.codeforces.com/contest/" + CONTEST_ID + "/problem/" + PROBLEM_ID
+req = requests.get(url)
+if not req.ok:
+    print("Could not find the problem page!")
+    sys.exit(-1)
+
+soup = BeautifulSoup(req.content, 'html.parser')
+case_div = soup.find("div", {"class": "sample-test"})
+
+
+input_div = case_div.find_all("div", {"class": "input"})
+case_cnt = len(input_div)
+single_case = True
+
+if case_cnt > 1:
+    single_case = False
+
+in_file = open(f"{PATH}/{PROBLEM_ID}.in", "w")
+
+for input in input_div:
+    input = input.pre
+
+    final = ""
+    for element in input:
+        inp = element.text
+
+        if case_cnt == 1:
+            in_file.write(inp.strip() + "\n")
+        else:
+            inp = inp.replace("\n", " ").strip()
+            final += inp + " "
+
+    if case_cnt > 1:
+        in_file.write(final + "\n")
+
+in_file.close()
+
+output_div = case_div.find_all("div", {"class": "output"})
+
+out_file = open(f"{PATH}/{PROBLEM_ID}.out", "w")
+for out in output_div:
+    out = out.pre
+    for br in out.find_all("br"):
+        br.replace_with("\n")
+
+    res = out.text
+    if case_cnt > 1:
+        res = res.replace("\n", " ")
+
+    out_file.write(res + "\n")
+
+settings["PROBLEM_ID"] = PROBLEM_ID
+settings["case cnt"] = case_cnt
+settings["single case"] = single_case
+
+f = open(f"{PATH}/config.cfg", "wb")
+pickle.dump(settings, f)
+f.close()
--- runner.py
+++ runner.py
@@ -4,8 +4,10 @@ import pickle
 import subprocess
 import time
 
+PATH = os.getcwd()
+
 # load in the settings
-f = open(".config.cfg", "rb")
+f = open(f"{PATH}/config.cfg", "rb")
 settings = pickle.load(f)
 f.close()
 
@@ -16,7 +18,7 @@ FAIL = '\033[91m'
 
 PROBLEM_ID = settings["PROBLEM_ID"]
 
-out_file = open(f"{PROBLEM_ID}.out", "r")
+out_file = open(f"{PATH}/{PROBLEM_ID}.out", "r")
 ans = []
 for line in out_file:
     s = line.strip().replace('\n', "")
@@ -25,7 +27,7 @@ for line in out_file:
 out_file.close()
 
 print(f"[DEBUG] Compiling {PROBLEM_ID}.cpp")
-os.system(f"g++ {PROBLEM_ID}.cpp -std=c++17 -o {PROBLEM_ID}")
+os.system(f"g++ {PATH}/{PROBLEM_ID}.cpp -std=c++17 -o {PATH}/{PROBLEM_ID}")
 result = []
 delta = 0
 
@@ -33,19 +35,19 @@ case_cnt = settings["case cnt"]
 
 if settings["single case"]:
     start = time.perf_counter()
-    out = subprocess.check_output(f"./{PROBLEM_ID} < {PROBLEM_ID}.in", shell=True, universal_newlines=True)
+    out = subprocess.check_output(f"{PATH}/{PROBLEM_ID} < {PATH}/{PROBLEM_ID}.in", shell=True, universal_newlines=True)
     delta = time.perf_counter() - start
     result = out.splitlines()
 else:
     inp = []
-    in_file = open(f"{PROBLEM_ID}.in", "r")
+    in_file = open(f"{PATH}/{PROBLEM_ID}.in", "r")
 
     for line in in_file:
         inp.append(line)
 
     for i in range(case_cnt):
         start = time.perf_counter()
-        out = subprocess.run([f"./{PROBLEM_ID}"], stdout=subprocess.PIPE, text=True, input=inp[i])
+        out = subprocess.run([f"{PATH}/{PROBLEM_ID}"], stdout=subprocess.PIPE, text=True, input=inp[i])
         temp_delta = time.perf_counter() - start
 
         delta += temp_delta
